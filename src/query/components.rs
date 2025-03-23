d9e3e8e43c38d8683ef842e454131531ef98336d
Kleo Davidson <odavidson@hpe.com>
2025-03-22 19:12:13 -0600

    feat: havingspec

--- src/query/components.rs
+++ src/query/components.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use crate::query::FloatingPointNumber;
 use crate::query::model::{NativeQuery, IntegerNumber};
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -289,8 +290,67 @@ pub struct OrderByColumnSpec {
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct Having {
-    //TODO
+#[serde(rename_all="camelCase")]
+pub enum HavingType {
+    Filter,
+    EqualTo,
+    GreaterThan,
+    LessThan,
+    DimSelector,
+    And,
+    Or,
+    Not,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum Having {
+    Filter {
+        #[serde(rename="type")]
+        type_: HavingType,
+        filter: Filter,
+    },
+    EqualTo {
+        #[serde(rename="type")]
+        type_: HavingType,
+        aggregation: String,
+        value: FloatingPointNumber
+    },
+    GreaterThan {
+        #[serde(rename="type")]
+        type_: HavingType,
+        aggregation: String,
+        value: FloatingPointNumber
+    },
+    LessThan {
+        #[serde(rename="type")]
+        type_: HavingType,
+        aggregation: String,
+        value: FloatingPointNumber
+    },
+    DimSelector {
+        #[serde(rename="type")]
+        type_: HavingType,
+        dimension: String,
+        value: Dimension,
+    },
+    #[serde(rename_all="camelCase")]
+    And {
+        #[serde(rename="type")]
+        type_: HavingType,
+        having_specs: Vec<Box<Having>>
+    },
+    #[serde(rename_all="camelCase")]
+    Or {
+        #[serde(rename="type")]
+        type_: HavingType,
+        having_specs: Vec<Box<Having>>
+    },
+    #[serde(rename_all="camelCase")]
+    Not {
+        #[serde(rename="type")]
+        type_: HavingType,
+        having_specs: Vec<Box<Having>>
+    },
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
